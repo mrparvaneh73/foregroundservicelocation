dece1f8608c4accc4d39f95e529f270fdb86d8fc
milad parvaneh <parvanehmilad73@yahoo.com>
2022-04-14 15:28:50 +0430

    Initial commit

---
+++ app/src/main/java/com/example/userlocation/LocationService.kt
@@ -0,0 +1,105 @@
+package com.example.userlocation
+
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+import com.google.android.gms.location.LocationRequest.create
+
+class LocationService : Service() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val locationRequest: LocationRequest = create().apply {
+        interval = 3000
+        fastestInterval = 3000
+        priority = PRIORITY_BALANCED_POWER_ACCURACY
+        maxWaitTime = 5000
+    }
+
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                val location = locationList.last()
+                Toast.makeText(this@LocationService, "Latitude: " + location.latitude.toString() + '\n' +
+                        "Longitude: "+ location.longitude, Toast.LENGTH_LONG).show()
+                Log.d("Location d", location.latitude.toString())
+                Log.i("Location i", location.latitude.toString())
+            }
+        }
+    }
+
+    /*******************/
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "location service: "+"location service is created")
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) createNotificationChanel() else startForeground(1, Notification())
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(applicationContext, "Permission required", Toast.LENGTH_LONG).show()
+            return
+        }else{
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChanel() {
+        val notificationChannelId = "Location channel id"
+        val channelName = "Background Service"
+        val chan = NotificationChannel(notificationChannelId, channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val manager = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+        manager.createNotificationChannel(chan)
+        val notificationBuilder =
+            NotificationCompat.Builder(this, notificationChannelId)
+        val notification: Notification = notificationBuilder.setOngoing(true)
+            .setContentTitle("Location updates:")
+            .setPriority(NotificationManager.IMPORTANCE_MIN)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .build()
+
+        startForeground(2, notification)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: "+"service destroied")
+        super.onDestroy()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+}
---
+++ app/src/main/java/com/example/userlocation/Util.kt
@@ -0,0 +1,23 @@
+package com.example.userlocation
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.util.Log
+
+object Util {
+//checking a service is running or not
+    fun isMyServiceRunning(serviceClass: Class<*>, mActivity: Activity): Boolean {
+        val manager: ActivityManager =
+            mActivity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                Log.i("Service status", "Running")
+                return true
+            }
+        }
+        Log.i("Service status", "Not running")
+        return false
+    }
+
+}
